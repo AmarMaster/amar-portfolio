a668a0d752e2867c5f98d33cda8a2d211fd0e7cf
Amar Master <akmaster2708@gmail.com>
2024-07-20 17:34:26 +0530

    finished work page and started contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '(+1) 413 949 3383'
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'amar.master.work@gmail.com'
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: 'Address',
+    description: 'Washington Hall - UMass, 181 Fearing Street, Amherst, MA'
+  }
+]
 
 export const Contact = () => {
-  return (
-    <div>contact page</div>
-  )
-}
+  return <motion.section>contact page</motion.section>;
+};
 
 export default Contact;
