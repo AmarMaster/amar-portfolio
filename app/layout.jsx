dad86a68b16cf8a4060a72fb9479b531ac4b08d5
Amar Master <akmaster2708@gmail.com>
2024-07-26 00:32:39 +0530

    final changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 // components
