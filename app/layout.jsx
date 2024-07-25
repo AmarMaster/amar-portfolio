68e0b26c06030e67cdadf88e4f7fe0ecd2db0ad3
Amar Master <akmaster2708@gmail.com>
2024-07-25 23:47:53 +0530

    vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { JetBrains_Mono } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 // components
@@ -24,9 +24,8 @@ export default function RootLayout({ children }) {
       <body className={jetBrainsMono.variable}>
         <Header />
         <StairTransition />
-        <PageTransition>
-        {children}
-        </PageTransition>
+        <PageTransition>{children}</PageTransition>
+        <Analytics />
       </body>
     </html>
   );
