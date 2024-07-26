400126e462ae5791303f05d8b4f993e84edb21c2
Amar Master <akmaster2708@gmail.com>
2024-07-26 15:51:56 +0530

    mobile nav auto close

--- components/ui/MobileNav.jsx
+++ components/ui/MobileNav.jsx
@@ -4,6 +4,8 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
+import { Button } from "./button";
 
 const links = [
   {
@@ -22,18 +24,20 @@ const links = [
     name: "projects",
     path: "/projects",
   },
-  {
-    name: "contact",
-    path: "/contact",
-  },
 ];
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger className=" flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent"></CiMenuFries>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         {/* logo */}
@@ -46,17 +50,19 @@ const MobileNav = () => {
         </div>
         {/* nav */}
         <nav className="flex flex-col justify-center items-center gap-8">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              onClick={handleLinkClick}
+              className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+            >
+              {link.name}
+            </Link>
+          ))}
+          <Link href="https://calendly.com/amarmaster">
+            <Button>Meet with Me</Button>
+          </Link>
         </nav>
       </SheetContent>
     </Sheet>
